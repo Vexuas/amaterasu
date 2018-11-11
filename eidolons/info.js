ce63d66effcff3455f17ad80b0fcf84f18f11aef
Vexuas <rodriguezgab1601@gmail.com>
2018-11-12 02:21:21 +0800

    change info to guide instead of list again

--- eidolons/info.js
+++ eidolons/info.js
@@ -1,14 +1,14 @@
 const embed = {
   "title": "Eternia || Info",
-  "description": "Eidolon Prayers List For Aura Kingdom.\n Made on the sole intention to remind my lazy self to do them.\n[Full list of prayers](http://www.aurakingdom-db.com/charts/eidolon-wishes)\n[Invite Link](https://discordapp.com/oauth2/authorize?&client_id=510980011008983060&scope=bot&permissions=8)",
+  "description": "Eidolon Prayers Guide For Aura Kingdom.\n Made on the sole intention to remind my lazy self to do them.\n[Full list of prayers](http://www.aurakingdom-db.com/charts/eidolon-wishes)\n[Invite Link](https://discordapp.com/oauth2/authorize?&client_id=510980011008983060&scope=bot&permissions=8)",
   "color": 63999,
   "thumbnail": {
     "url": "https://cdn.discordapp.com/attachments/248430185463021569/511047719851130890/discord_eternia.jpg"
   },
   "fields": [
     {
-      "name": "Commands",
-      "value": "**$eidolons**  :  Displays all available Eidolons in the server.\n**$EidolonName**  :  Displays specific Eidolon prayers (e.g.: $Merilee).\n**$dig**  :  Displays current day archaeology sites for specific Eidolons.\n**$fish** :  Displays current day fish kings for specific Eidolons."
+      "name": "List of Commands",
+      "value": "**$eidolons**  :  Displays all available Eidolons in the server.\n**$EidolonName**  :  Displays specific Eidolon prayers (e.g.: $Merrilee).\n**$dig**  :  Displays current day archaeology sites for specific Eidolons.\n**$fish** :  Displays current day fish kings for specific Eidolons."
     },
     {
       "name": "Contact",
