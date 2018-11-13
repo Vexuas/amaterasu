7de7bf8d856d21194b03b2d0d6f47c2096bcd9d0
Vexuas <rodriguezgab1601@gmail.com>
2018-11-13 14:42:35 +0800

    new invite command

---
+++ eidolons/invite.js
@@ -0,0 +1,13 @@
+const embed = {
+  "title": "Eternia Bot Invite Link",
+  "description": "[Add me to your servers! (~'-')~](https://discordapp.com/oauth2/authorize?&client_id=510980011008983060&scope=bot&permissions=8)",
+  "color": 1493680
+};
+
+module.exports = {
+  name: 'invite',
+  description: 'invite link',
+  execute(message, args) {
+    message.channel.send({ embed });
+  },
+};
