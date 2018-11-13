4d21a64257d825c09e09f081a4d14ee9b891a951
Vexuas <rodriguezgab1601@gmail.com>
2018-11-13 14:43:00 +0800

    update description of bot and overall better layout

--- eidolons/info.js
+++ eidolons/info.js
@@ -1,18 +1,17 @@
 const embed = {
   "title": "Eternia || Info",
-  "description": "Eidolon Prayers Guide For Aura Kingdom.\n Made on the sole intention to remind my lazy self to do them.\n[Full list of prayers](http://www.aurakingdom-db.com/charts/eidolon-wishes)\n[Invite Link](https://discordapp.com/oauth2/authorize?&client_id=510980011008983060&scope=bot&permissions=8)",
+  "description": "Eidolon Bot For Aura Kingdom.\n Initially made as an Eidolon Prayer Guide to remind my lazy self to do them but went ahead and added more functionality.\n[Invite Link](https://discordapp.com/oauth2/authorize?&client_id=510980011008983060&scope=bot&permissions=8)",
   "color": 1493680,
+  "footer": {
+    "text": "If you happen to find missing eidolons/items or mistakes in general, feel free to add Vexuas#8141 or contact me through the official AKUS Discord.  (´･ᴗ･`)"
+  },
   "thumbnail": {
     "url": "https://cdn.discordapp.com/attachments/248430185463021569/511047719851130890/discord_eternia.jpg"
   },
   "fields": [
     {
       "name": "List of Commands",
-      "value": "**$eidolons**  :  Displays all available Eidolons in the server.\n**$EidolonName**  :  Displays specific Eidolon prayers (e.g.: $Merrilee).\n**$dig**  :  Displays current day archaeology sites for specific Eidolons.\n**$fish** :  Displays current day fish kings for specific Eidolons.\n**$stats** : Displays stats from prayers"
-    },
-    {
-      "name": "Contact",
-      "value": "If you happen to find missing eidolons/items or mistakes in general, feel free to add `Vexuas#8141` or contact me through the official AKUS Discord."
+      "value": "**$`info`** : Displays bot information.\n**$`invite`** : Generates invite link.\n**$`eidolons `**  :  Displays all available Eidolons in the server.\n**$`EidolonName`**  :  Displays Eidolon description. \n**$`prayers EidolonName`** : Displays specific Eidolon prayers.\n**$`dig`**  :  Displays current day archaeology sites for specific Eidolons.\n**$`fish`** :  Displays current day fish kings for specific Eidolons.\n**$`stats`** : Displays stats from prayers.\n\n```fix\nType $ at the beginning of any command: $Merrilee, $prayers Merrilee, $stats .. ```"
     }
   ]
 };
