c75cd6fd642ca9f426a3afa3d89e4b34f965d7df
Vexuas <rodriguezgab1601@gmail.com>
2018-11-13 11:46:51 +0800

    update bot variable name

--- Eternia.js
+++ Eternia.js
@@ -2,35 +2,35 @@ const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json')
 
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
+const eternia = new Discord.Client();
+eternia.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./eidolons').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
   const command = require(`./eidolons/${file}`);
-  client.commands.set(command.name, command);
+  eternia.commands.set(command.name, command);
 }
 
-client.once('ready', () => {
+eternia.once('ready', () => {
   console.log('Ready!');
 });
 
-client.on('ready', () => {
-  client.user.setActivity("$info for help || (~'-')~♥")
+eternia.on('ready', () => {
+  eternia.user.setActivity("$info for help || (~'-')~♥")
 });
 
-client.login(token);
+eternia.login(token);
 
-client.on('message', message => {
+eternia.on('message', message => {
   if (!message.content === prefix) return;
 
   const args = message.content.slice(prefix.length).split(/ +/);
   const command = args.shift().toLowerCase();
 
-  if (!client.commands.has(command)) return;
+  if (!eternia.commands.has(command)) return;
   try {
-    client.commands.get(command).execute(message, args);
+    eternia.commands.get(command).execute(message, args);
   }
   catch (error) {
     console.error(error);
