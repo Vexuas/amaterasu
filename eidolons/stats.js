c8c43b26a7c480a3eaf2e35130eeae52009c2373
Vexuas <rodriguezgab1601@gmail.com>
2018-11-13 13:24:09 +0800

    improve stats layout

--- eidolons/stats.js
+++ eidolons/stats.js
@@ -6,20 +6,24 @@ const embed = {
   },
   "fields": [
     {
-      "name": "1st Wish + 2nd Wish",
-      "value": "```fix\n• DMG +36    • CRIT +14\n• SPD +12    • HP +44\n• DEF +6     • EVA +16```"
+      "name": "1st & 2nd Wish",
+      "value": "```fix\n• DMG +36    • CRIT +14\n• SPD +12    • HP +44\n• DEF +6     • EVA +16```",
+      "inline": true
     },
     {
-      "name": "3rd Wish + 4th Wish",
-      "value": "```fix\n• DMG +72    • CRIT +28 \n• SPD +22    • HP +88\n• DEF +14    • EVA +40```"
+      "name": "3rd & 4th Wish",
+      "value": "```fix\n• DMG +72    • CRIT +28 \n• SPD +22    • HP +88\n• DEF +14    • EVA +40```",
+      "inline": true
     },
     {
-      "name": "5th Wish + 6th",
-      "value": "```fix\n• DMG +130   • CRIT +50\n• SPD +42    • HP +158\n• DEF +26    • EVA +66```"
+      "name": "5th & 6th Wish",
+      "value": "```fix\n• DMG +130   • CRIT +50\n• SPD +42    • HP +158\n• DEF +26    • EVA +66```",
+      "inline": true
     },
     {
       "name": "Total",
-      "value": "```xl\nDMG : 238    CRIT: 92\nSPD : 76     HP  : 290\nDEF : 46     EVA : 119```"
+      "value": "```xl\nDMG : 238    CRIT: 92   \nSPD : 76     HP  : 290\nDEF : 46     EVA : 119```",
+      "inline": true
     }
   ]
 };
