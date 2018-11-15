d15c306cf8449d178219fae03cd4c368d4332cd8
Vexuas <rodriguezgab1601@gmail.com>
2018-11-15 20:04:33 +0800

    hansel and gretel rename

--- eidolons/hansel&gretel.js
+++ eidolons/hansel&gretel.js
@@ -1,5 +1,5 @@
 const embed = {
-  "title": "Hansel & Gretel",
+  "title": "Hansel and Gretel",
   "description": "Hansel and Gretel were drawn to Terra via the Power of Gaia. With the blessing of the forest at their command, they harness the energy of storm and wind. Hansel possesses a keen sense of smell and the ability to vanquish gigantic foes. Gretel, on the other hand, is gifted with tremendous healing abilities.\n\nWhen they blow their horn, they are able to restore their allies' health, allowing them to keep fighting the good fight.\n[Artwork](http://aurakingdom.wikia.com/wiki/File:Hansel_%26_Gretel_Wallpaper.png)",
   "color": 7667557,
   "thumbnail": {
@@ -34,7 +34,7 @@ const embed = {
 };
 
 module.exports = {
-  name: 'hansel & gretel',
+  name: 'hansel and gretel',
   description: 'hansel prayers',
   execute(message, args) {
     message.channel.send({ embed });
