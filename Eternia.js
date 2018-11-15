981df3a97c5499e19d30af3cc309789041e658c5
Vexuas <rodriguezgab1601@gmail.com>
2018-11-15 14:25:36 +0800

    fixed issue of server time not updating, transfer code from dig to main

--- Eternia.js
+++ Eternia.js
@@ -1,6 +1,21 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json')
+const sunDig = require('./eidolons/dig-sunday');
+const monDig = require('./eidolons/dig-monday');
+const tueDig = require('./eidolons/dig-tuesday');
+const wedDig = require('./eidolons/dig-wednesday');
+const thursDig = require('./eidolons/dig-thursday');
+const friDig = require('./eidolons/dig-friday');
+const satDig = require('./eidolons/dig-saturday');
+const sunday = sunDig.day;
+const monday = monDig.day;
+const tuesday = tueDig.day;
+const wednesday = wedDig.day;
+const thursday = thursDig.day;
+const friday = friDig.day;
+const saturday = satDig.day;
+
 
 const eternia = new Discord.Client();
 eternia.commands = new Discord.Collection();
@@ -25,11 +40,54 @@ eternia.login(token);
 eternia.on('message', message => {
   if (message.content === "Rio's not here") return message.channel.send("R-really? She forced me to say it! She's actually so smol!! (๑•́ω•̀)");
   if (!message.content.startsWith(prefix)) return;
+  //Gets Current Date
+  const d = new Date();
+  //Converts date into universal time in milliseconds
+  const utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+  // EST = GMT -5
+  const offset = -5;
+  //Game Time in milliseconds, universal time + difference in hours from utc(-5)
+  const gameTime = utc + (3600000 * offset);
+  //Returns a full Date Object of server time
+  const serverTime = new Date(gameTime);
+  //Format to only time string; 06:42 AM 
+  const ServerTime = serverTime.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+  //Gets day from Date Object, returns a number(0-6)
+  const day = serverTime.getDay();
+
+  //digging sites, transfered from dig.js to Eternia.js to solve servertime issues
+  if (message.content === "$dig") {
+    let embed = {};
+    if (day === 0) {
+      embed = sunday;
+      embed.description = "Server Time : ` Sunday, " + ServerTime + "`";
+    } else if (day === 1) {
+      embed = monday;
+      embed.description = "Server Time : ` Monday, " + ServerTime + "`";
+    } else if (day === 2) {
+      embed = tuesday;
+      embed.description = "Server Time : ` Tuesday, " + ServerTime + "`";
+    } else if (day === 3) {
+      embed = wednesday;
+      embed.description = "Server Time : ` Wednesday, " + ServerTime + "`";
+    } else if (day === 4) {
+      embed = thursday;
+      embed.description = "Server Time: `Thursday, " + ServerTime + "`";
+    } else if (day === 5) {
+      embed = friday;
+      embed.description = "Server Time: ` Friday, " + ServerTime + "`";
+    } else if (day === 6) {
+      embed = saturday;
+      embed.description = "Server Time: ` Saturday, " + ServerTime + "`";
+    };
+    message.channel.send("Archaeology Prayer Sites Today " + message.author, { embed })
+  };
 
   const args = message.content.slice(prefix.length).split();
   const command = args.shift().toLowerCase();
 
   if (!eternia.commands.has(command)) return message.channel.send("Master " + message.author.username + ", I'm not sure what you meant by that! （・□・；）");
+
   try {
     eternia.commands.get(command).execute(message, args);
   }
--- eidolons/dig.js
+++ eidolons/dig.js
@@ -1,62 +1,6 @@
-const sun = require('./dig-sunday');
-const mon = require('./dig-monday');
-const tue = require('./dig-tuesday');
-const wed = require('./dig-wednesday');
-const thurs = require('./dig-thursday');
-const fri = require('./dig-friday');
-const sat = require('./dig-saturday');
-const sunday = sun.day;
-const monday = mon.day;
-const tuesday = tue.day;
-const wednesday = wed.day;
-const thursday = thurs.day;
-const friday = fri.day;
-const saturday = sat.day;
-
-//Gets Current Date
-const d = new Date();
-//Converts date into universal time in milliseconds
-const utc = d.getTime() + (d.getTimezoneOffset() * 60000);
-// EST = GMT -5
-const offset = -5;
-//Game Time in milliseconds, universal time + difference in hours from utc(-5)
-const gameTime = utc + (3600000 * offset);
-//Returns a full Date Object of server time
-const serverTime = new Date(gameTime);
-//Format to only time string; 06:42 AM 
-const ServerTime = serverTime.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
-//Gets day from Date Object, returns a number(0-6)
-const day = serverTime.getDay();
-
-let embed = {};
-if (day === 0) {
-  embed = sunday;
-  embed.description = "Server Time : ` Sunday, " + ServerTime + "`";
-} else if (day === 1) {
-  embed = monday;
-  embed.description = "Server Time : ` Monday, " + ServerTime + "`";
-} else if (day === 2) {
-  embed = tuesday;
-  embed.description = "Server Time : ` Tuesday, " + ServerTime + "`";
-} else if (day === 3) {
-  embed = wednesday;
-  embed.description = "Server Time : ` Wednesday, " + ServerTime + "`";
-} else if (day === 4) {
-  embed = thursday;
-  embed.description = "Server Time: `Thursday, " + ServerTime + "`";
-} else if (day === 5) {
-  embed = friday;
-  embed.description = "Server Time: ` Friday, " + ServerTime + "`";
-} else if (day === 6) {
-  embed = saturday;
-  embed.description = "Server Time: ` Saturday, " + ServerTime + "`";
-};
-
-
+//switched codes to main file to fix issue of server time
 module.exports = {
   name: 'dig',
   description: 'digging sites for eidolons',
-  execute(message, args) {
-    message.channel.send("Archaeology Prayer Sites Today " + message.author, { embed })
-  }
+  execute(message, args) { }
 };
