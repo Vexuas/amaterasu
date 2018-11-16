d8ff582342caa1eccd08dc9f25f27cc241d00e19
Vexuas <rodriguezgab1601@gmail.com>
2018-11-16 21:39:17 +0800

    placeholders for new commands

---
+++ eidolons/prayers.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'prayers',
+  description: 'prayers info',
+  execute(message, args) {
+    message.channel.send("This is a work-in-progress! (ง •̀ω•́)ง✧")
+  }
+};
---
+++ eidolons/spawns.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'spawns',
+  description: 'eido dungeon spawns',
+  execute(message, args) {
+    message.channel.send("This is a work-in-progress! (ง •̀ω•́)ง✧")
+  },
+};
