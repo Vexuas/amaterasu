9de03a4aa8c3bf336261d011531cc51091598187
Vexuas <rodriguezgab1601@gmail.com>
2018-11-16 15:30:31 +0800

    new custom reactions + fish king command

--- Eternia.js
+++ Eternia.js
@@ -8,13 +8,25 @@ const wedDig = require('./eidolons/dig-wednesday');
 const thursDig = require('./eidolons/dig-thursday');
 const friDig = require('./eidolons/dig-friday');
 const satDig = require('./eidolons/dig-saturday');
-const sunday = sunDig.day;
-const monday = monDig.day;
-const tuesday = tueDig.day;
-const wednesday = wedDig.day;
-const thursday = thursDig.day;
-const friday = friDig.day;
-const saturday = satDig.day;
+const sunFish = require('./eidolons/fish-sunday');
+const monFish = require('./eidolons/fish-monday');
+const wedFish = require('./eidolons/fish-wednesday');
+const thursFish = require('./eidolons/fish-thursday');
+const friFish = require('./eidolons/fish-friday');
+const satFish = require('./eidolons/fish-saturday');
+const sundayD = sunDig.day;
+const mondayD = monDig.day;
+const tuesdayD = tueDig.day;
+const wednesdayD = wedDig.day;
+const thursdayD = thursDig.day;
+const fridayD = friDig.day;
+const saturdayD = satDig.day;
+const sundayF = sunFish.day;
+const mondayF = monFish.day;
+const wednesdayF = wedFish.day;
+const thursdayF = thursFish.day;
+const fridayF = friFish.day;
+const saturdayF = satFish.day;
 
 
 const eternia = new Discord.Client();
@@ -38,7 +50,17 @@ eternia.on('ready', () => {
 
 eternia.login(token);
 eternia.on('message', message => {
+  //custom reactions
   if (message.content === "Rio's not here") return message.channel.send("R-really? She forced me to say it! She's actually so smol!! (๑•́ω•̀)");
+  if (message.author.username === "Vexuas" && message.content === "I love you " + eternia.user) {
+    message.channel.send("I love you too Master Vex! (˶◕‿◕˶✿)");
+  } else if (message.author.username === "Rio" && message.content === "I love you " + eternia.user) {
+    message.channel.send("I love you too ArmRest-chan (˶◕‿◕˶✿)");
+  } else if (message.author.username !== "Vexuas" && message.author.username !== "Rio" && message.content === "I love you " + eternia.user) {
+    message.channel.send("U-uhh, thanks! You're nice too! （°o°；）");
+  };
+
+  //If no $, do nothing
   if (!message.content.startsWith(prefix)) return;
   //Gets Current Date
   const d = new Date();
@@ -59,35 +81,64 @@ eternia.on('message', message => {
   if (message.content === "$dig") {
     let embed = {};
     if (day === 0) {
-      embed = sunday;
+      embed = sundayD;
       embed.description = "Server Time : ` Sunday, " + ServerTime + "`";
     } else if (day === 1) {
-      embed = monday;
+      embed = mondayD;
       embed.description = "Server Time : ` Monday, " + ServerTime + "`";
     } else if (day === 2) {
-      embed = tuesday;
+      embed = tuesdayD;
       embed.description = "Server Time : ` Tuesday, " + ServerTime + "`";
     } else if (day === 3) {
-      embed = wednesday;
+      embed = wednesdayD;
       embed.description = "Server Time : ` Wednesday, " + ServerTime + "`";
     } else if (day === 4) {
-      embed = thursday;
+      embed = thursdayD;
       embed.description = "Server Time: `Thursday, " + ServerTime + "`";
     } else if (day === 5) {
-      embed = friday;
+      embed = fridayD;
       embed.description = "Server Time: ` Friday, " + ServerTime + "`";
     } else if (day === 6) {
-      embed = saturday;
+      embed = saturdayD;
       embed.description = "Server Time: ` Saturday, " + ServerTime + "`";
     };
     message.channel.send("Archaeology Prayer Sites Today " + message.author, { embed })
   };
 
+  //fishing sites
+  if (message.content === "$fish") {
+    let embed = {};
+    if (day === 0) {
+      embed = sundayF;
+      embed.description = "Server Time : ` Sunday, " + ServerTime + "`";
+    } else if (day === 1) {
+      embed = mondayF;
+      embed.description = "Server Time : ` Monday, " + ServerTime + "`";
+    } else if (day === 2) {
+      message.channel.send("No Fish King Prayers Today (´･ᴗ･`)" + message.author);
+    } else if (day === 3) {
+      embed = wednesdayF;
+      embed.description = "Server Time : ` Wednesday, " + ServerTime + "`";
+    } else if (day === 4) {
+      embed = thursdayF;
+      embed.description = "Server Time: `Thursday, " + ServerTime + "`";
+    } else if (day === 5) {
+      embed = fridayF;
+      embed.description = "Server Time: ` Friday, " + ServerTime + "`";
+    } else if (day === 6) {
+      embed = saturdayF;
+      embed.description = "Server Time: ` Saturday, " + ServerTime + "`";
+    };
+    message.channel.send("Fish King Prayer Locations Today " + message.author, { embed })
+  };
+
+  //get commands
   const args = message.content.slice(prefix.length).split();
   const command = args.shift().toLowerCase();
 
+  //if command doesn't exist, show error message
   if (!eternia.commands.has(command)) return message.channel.send("Master " + message.author.username + ", I'm not sure what you meant by that! （・□・；）");
-
+  //if it does, do command
   try {
     eternia.commands.get(command).execute(message, args);
   }
