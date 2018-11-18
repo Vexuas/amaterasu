9242053a682481612de0ca2b8797c331d98b1032
Vexuas <rodriguezgab1601@gmail.com>
2018-11-19 02:38:27 +0800

    change title

--- eidolons/eidolons.js
+++ eidolons/eidolons.js
@@ -3,7 +3,7 @@ const embed = {
   "color": 1493680,
   "fields": [
     {
-      "name": "Eidolon List",
+      "name": "Full Eidolon List",
       "value": "• `Abraxas` • `Aelius` • `Alessa` • `Alice` • `Alucard` • `Amaterasu` • `Astraea` • `Ayako` \n• `Bahadur` • `Bealdor` • `Bel-Chandra` • `Benkei` • `Cesela` • `Cleopawtra` • `Cyril`\n• `Dante` • `Demeter` • `Diao Chan` • `Eirene` • `Eligos` • `Endora` • `Eternia` • `Faust`\n• `Ghodroon` • `Gigas` • `Grimm` • `Hades` • `Hansel and Gretel` • `Harmonia` • `Hebe` • `Hel` • `Izanami` • `Justicia` • `Kaiser Zeta` • `Kitami` • `Kotonoha` • `Kusanagi` • `Lumikki`\n• `Maja` • `Merrilee` • `Michaela` • `Muramasa` • `Muse` • `Nalani` • `Nazrudin` • `Pandora`\n• `Quelkulan` • `Serena` • `Serif` • `Shirayuki` • `Sigrun` • `Tanith` • `Tigerius Caesar`\n• `Tsubaki` • `Tyr` • `Undine` • `Uriel` • `Uzuriel` • `Vayu` • `Venus` • `Verdandi`\n• `Vermilion` • `Yarnaros` • `Yumikaze` • `Zaahir` • `Zashi` • `Zephyrine`"
     }
   ]
