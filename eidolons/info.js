364b2aa66a4156ede2e0b9166b61a40d4cbe4ab3
Gabe R <rodriguezgab1601@gmail.com>
2019-02-10 00:12:57 +0800

    Update 09/12/2019 - Renamed to amaterasu and updated corresponding info

--- eidolons/info.js
+++ eidolons/info.js
@@ -1,25 +1,29 @@
 const embed = {
-  "title": "Eternia || Info",
-  "description": "Eidolon Bot For Aura Kingdom.\n Initially made as an Eidolon Prayer Guide to remind my lazy self to do them but went ahead and added more functionality.\n[Invite Link](https://discordapp.com/oauth2/authorize?&client_id=510980011008983060&scope=bot&permissions=8)",
-  "color": 1493680,
-  "footer": {
-    "text": "If you happen to find missing eidolons/items or mistakes in general, feel free to message Vexuas#8141 or contact me through the AKUS Discord.  (´･ᴗ･`) Last Update: 16/12/2018"
+  title: "Amaterasu || Info",
+  description:
+    "Eidolon Bot For Aura Kingdom.\n Initially made as an Eidolon Prayer Guide to remind my lazy self to do them but went ahead and added more functionality.\n[Invite Link](https://discordapp.com/oauth2/authorize?&client_id=510980011008983060&scope=bot&permissions=8)\n[Invite Link](placeholder) - Eternia(ak-timer bot) WIP",
+  color: 1493680,
+  footer: {
+    text:
+      "If you happen to find missing eidolons/items or mistakes in general, feel free to message Vexuas#8141 or contact me through the AKUS Discord.  (´･ᴗ･`) Last Update: 09/02/2019"
   },
-  "thumbnail": {
-    "url": "https://cdn.discordapp.com/attachments/248430185463021569/511047719851130890/discord_eternia.jpg"
+  thumbnail: {
+    url:
+      "https://cdn.discordapp.com/attachments/535291698046042112/543825946977763348/Amaterasu-new.png"
   },
-  "fields": [
+  fields: [
     {
-      "name": "List of Commands",
-      "value": "**$`info`** : Displays bot information.\n**$`invite`** : Generates invite link.\n**$`eidolons `**  :  Displays all available Eidolons in the server.\n**$`EidolonName`**  :  Displays Eidolon information. \n**$`prayers EidolonName`** : Displays specific Eidolon prayers.\n**$`dig`**  :  Displays current day archaeology sites for Eidolons.\n**$`dig Day`**  :  Displays specific day archaeology sites for Eidolons.\n**$`fish`** :  Displays current day fish kings for Eidolons.\n**$`fish Day`** :  Displays specific day fish kings for Eidolons.\n**$`spawns`** :  Displays Eidolons that can spawn in instances.\n**$`spawns EidolonName`** :  Displays instances of specific Eidolons.\n**$`stats`** : Displays stats from prayers.\n\n```fix\nType $ at the beginning of any command: $Merrilee, $prayers Merrilee, $stats .. ```"
+      name: "List of Commands",
+      value:
+        "**$`info`** : Displays bot information.\n**$`invite`** : Generates invite link.\n**$`eidolons `**  :  Displays all available Eidolons in the server.\n**$`EidolonName`**  :  Displays Eidolon information. \n**$`prayers EidolonName`** : Displays specific Eidolon prayers.\n**$`dig`**  :  Displays current day archaeology sites for Eidolons.\n**$`dig Day`**  :  Displays specific day archaeology sites for Eidolons.\n**$`fish`** :  Displays current day fish kings for Eidolons.\n**$`fish Day`** :  Displays specific day fish kings for Eidolons.\n**$`spawns`** :  Displays Eidolons that can spawn in instances.\n**$`spawns EidolonName`** :  Displays instances of specific Eidolons.\n**$`stats`** : Displays stats from prayers.\n\n```fix\nType $ at the beginning of any command: $Merrilee, $prayers Merrilee, $stats .. ```"
     }
   ]
 };
 
 module.exports = {
-  name: 'info',
-  description: 'bot information',
+  name: "info",
+  description: "bot information",
   execute(message, args) {
     message.channel.send({ embed });
-  },
+  }
 };
