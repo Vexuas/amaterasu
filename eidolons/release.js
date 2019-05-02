c816f16e1821fcbbbf4dca6287af9a98080a85f0
Gabriel R <42207245+Vexuas@users.noreply.github.com>
2019-05-03 01:18:53 +0800

    List release changelog (#14)

--- eidolons/release.js
+++ eidolons/release.js
@@ -2,9 +2,12 @@ module.exports = {
   name: "release",
   description: "latest release information",
   execute(message, args) {
-    const release = "```• Added $release command```";
+    const release =
+      "```• Added $release command\n• Added Cerberus\n• Added Nidhogg```";
     const embed = {
-      description: `${message.author} | Latest Release: v1.74\n${release}`,
+      description: `${
+        message.author
+      } | Latest Release: [v1.75](https://github.com/Vexuas/amaterasu/releases)\n${release}`,
       color: 1493680
     };
     message.channel.send({ embed });
