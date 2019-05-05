93b72ab2bf3cf5e1e0fe02335f8a546b4aaeafef
Gabriel R <42207245+Vexuas@users.noreply.github.com>
2019-05-06 02:08:28 +0800

    Release/1.76 (#17)
    * Add contact information at footer (#11)
    
    * Feature/release embed (#12)
    
    * Add release changelog embed
    
    * Add release command in info
    
    * Update readme
    
    * Update changelog
    
    * List release changelog (#14)
    
    * Feature/contacts (#15)
    
    * Add contacts embed
    
    * Update info embed with contacts command and remove footer
    
    * Edit update date
    
    * Bump up version number and update current changelog

--- eidolons/release.js
+++ eidolons/release.js
@@ -2,12 +2,11 @@ module.exports = {
   name: "release",
   description: "latest release information",
   execute(message, args) {
-    const release =
-      "```• Added $release command\n• Added Cerberus\n• Added Nidhogg```";
+    const release = "```• Added $contacts command```";
     const embed = {
       description: `${
         message.author
-      } | Latest Release: [v1.75](https://github.com/Vexuas/amaterasu/releases)\n${release}`,
+      } | Latest Release: [v1.76](https://github.com/Vexuas/amaterasu/releases)\n${release}`,
       color: 1493680
     };
     message.channel.send({ embed });
