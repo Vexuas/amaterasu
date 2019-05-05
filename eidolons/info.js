dd4c30264f669ace8c2ef16b59212a59d4b53401
Gabe R <rodriguezgab1601@gmail.com>
2019-05-06 01:49:02 +0800

    Update info embed with contacts command and remove footer

--- eidolons/info.js
+++ eidolons/info.js
@@ -7,15 +7,11 @@ const embed = {
     url:
       "https://cdn.discordapp.com/attachments/535291698046042112/543825946977763348/Amaterasu-new.png"
   },
-  footer: {
-    text:
-      "If you happen to find missing eidolons/items or mistakes in general, feel free to message me at Vexuas#8141 or contact me through the AKUS discord  (´･ᴗ･`)"
-  },
   fields: [
     {
       name: "List of Commands",
       value:
-        "**$`info`** : Displays bot information.\n**$`invite`** : Generates invite link.\n**$`eidolons `**  :  Displays all available Eidolons in the server.\n**$`EidolonName`**  :  Displays Eidolon information. \n**$`prayers EidolonName`** : Displays specific Eidolon prayers.\n**$`dig`**  :  Displays current day archaeology sites for Eidolons.\n**$`dig Day`**  :  Displays specific day archaeology sites for Eidolons.\n**$`fish`** :  Displays current day fish kings for Eidolons.\n**$`fish Day`** :  Displays specific day fish kings for Eidolons.\n**$`spawns`** :  Displays Eidolons that can spawn in instances.\n**$`spawns EidolonName`** :  Displays instances of specific Eidolons.\n**$`stats`** : Displays stats from prayers.\n**$`release`** : Displays current release changelog."
+        "**$`info`** : Displays bot information.\n**$`invite`** : Generates invite link.\n**$`eidolons `**  :  Displays all available Eidolons in the server.\n**$`EidolonName`**  :  Displays Eidolon information. \n**$`prayers EidolonName`** : Displays specific Eidolon prayers.\n**$`dig`**  :  Displays current day archaeology sites for Eidolons.\n**$`dig Day`**  :  Displays specific day archaeology sites for Eidolons.\n**$`fish`** :  Displays current day fish kings for Eidolons.\n**$`fish Day`** :  Displays specific day fish kings for Eidolons.\n**$`spawns`** :  Displays Eidolons that can spawn in instances.\n**$`spawns EidolonName`** :  Displays instances of specific Eidolons.\n**$`stats`** : Displays stats from prayers.\n**$`release`** : Displays current release changelog.\n**$`contacts`** : Displays where to contact owner."
     },
     {
       name: "Bot Links",
