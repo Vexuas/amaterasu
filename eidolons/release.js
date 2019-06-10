c734144212e95736c59dea8cfaa9295c1da7c70e
Gabe R <rodriguezgab1601@gmail.com>
2019-06-10 20:44:31 +0800

    Update release link

--- eidolons/release.js
+++ eidolons/release.js
@@ -1,12 +1,12 @@
 module.exports = {
-  name: "release",
-  description: "latest release information",
+  name: 'release',
+  description: 'latest release information',
   execute(message, args) {
-    const release = "```• New eidolon Festival Muramasa```";
+    const release = '```• New eidolon Festival Muramasa```';
     const embed = {
       description: `${
         message.author
-      } | Latest Release: [v1.79](https://github.com/Vexuas/amaterasu/releases)\n${release}`,
+      } | Latest Release: [v1.79](https://github.com/vexuas/amaterasu/releases)\n${release}`,
       color: 1493680
     };
     message.channel.send({ embed });
