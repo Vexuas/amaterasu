7995452f8b2bebb904de59748405bcd5d54f97d2
Gabriel R <42207245+vexuas@users.noreply.github.com>
2019-06-26 23:15:03 +0800

    New eidolon spawns in the new dragon dungeon (#45)

--- commands/instance_spawns/spawns-muse.js
+++ commands/instance_spawns/spawns-muse.js
@@ -1,21 +1,21 @@
 const embed = {
-  description: "Muse Spawns",
+  description: 'Muse Spawns',
   color: 1493680,
   thumbnail: {
-    url: "http://cdn.aurakingdom-db.com/images/icons/P00053.jpg"
+    url: 'http://cdn.aurakingdom-db.com/images/icons/P00053.jpg'
   },
   fields: [
     {
-      name: "Instances",
+      name: 'Instances',
       value:
-        "```css\n• Dawn Passage [Hell]\n• Silent Ancient Castle [Hell]         ```"
+        '```css\n• Dawn Passage [Hell]\n• Silent Ancient Castle [Hell]\n• Flowing Flame Forest             ```'
     }
   ]
 };
 
 module.exports = {
-  name: "spawns muse",
-  description: "muse spawns",
+  name: 'spawns muse',
+  description: 'muse spawns',
   execute(message, args) {
     embed.description = `${message.author} | Muse Spawns`;
     message.channel.send({ embed });
